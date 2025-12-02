9db2af624d4ffd6232711ce1819d4aef46c88c35
Zeran Lu <zeranlu0326@gmail.com>
2025-12-01 22:42:08 -0500

    Update main.js
    In this commit, the materials array was commented out, and instead, the Material List data is fetched from https://swiftpixel.com/earbud/api/materials

--- js/main.js
+++ js/main.js
@@ -14,134 +14,113 @@
   //This information needs to be removed then pulled with an AJAX Call using the Fetch API
   //this is the api url https://swiftpixel.com/earbud/api/infoboxes"
 
-  const infoBoxes = [
-    {
-      title: 'Noise-cancelling microphones',
-      text: 'Noise-cancelling microphones and a rear copper shield are optimally placed to quickly detect outside noises, working together to counter noise before it disturbs your experience.',
-      image: 'images/ear-piece.jpg'
-    },
-    {
-      title: 'Comfortable fit',
-      text: 'Three pairs of ultra comfortable silicone tips are included. The tips create an acoustic seal that blocks outside audio and secures the earbuds in place.',
-      image: 'images/ear-piece.jpg'
-    },
-    {
-      title: '360 AUDIO',
-      text: '360 Audio places sound all around you, while Dolby Head Tracking™ technology delivers an incredible three-dimensional listening experience.',
-      image: 'images/ear-piece.jpg'
-    },
-    {
-      title: 'Ultra Fast Charging',
-      text: 'Charge your earbuds in 30 minutes or less with our hyper charging technology.',
-      image: 'images/ear-piece.jpg'
-    },
-  ];
+  // const infoBoxes = [
+  //   {
+  //     title: 'Noise-cancelling microphones',
+  //     text: 'Noise-cancelling microphones and a rear copper shield are optimally placed to quickly detect outside noises, working together to counter noise before it disturbs your experience.',
+  //     image: 'images/ear-piece.jpg'
+  //   },
+  //   {
+  //     title: 'Comfortable fit',
+  //     text: 'Three pairs of ultra comfortable silicone tips are included. The tips create an acoustic seal that blocks outside audio and secures the earbuds in place.',
+  //     image: 'images/ear-piece.jpg'
+  //   },
+  //   {
+  //     title: '360 AUDIO',
+  //     text: '360 Audio places sound all around you, while Dolby Head Tracking™ technology delivers an incredible three-dimensional listening experience.',
+  //     image: 'images/ear-piece.jpg'
+  //   },
+  //   {
+  //     title: 'Ultra Fast Charging',
+  //     text: 'Charge your earbuds in 30 minutes or less with our hyper charging technology.',
+  //     image: 'images/ear-piece.jpg'
+  //   },
+  // ];
 
   //This information needs to be removed then pulled with an AJAX Call using the Fetch API
   //this is the api url https://swiftpixel.com/earbud/api/materials"
 
-  const materialListData = [
-    {
-      heading: "Precision-Crafted Polymers",
-      description: "Our earbuds are meticulously molded from high-quality plastics, ensuring a blend of elegance, comfort, and resilience that's second to none."
-    },
-    {
-      heading: "Luxurious Silicone Harmony",
-      description: "Our uniquely engineered ear tips are cocooned in plush silicone, delivering an opulent embrace for your ears, ensuring an unrivaled fit and exquisite audio experience."
-    },
-    {
-      heading: "Rubberized Cables",
-      description: "Experience the unparalleled freedom of movement with our flexible rubber cables that promise durability without compromise."
-    },
-    {
-      heading: "Enhanced Comfort Sensors",
-      description: "A touch of magic in the form of built-in microphones and sensors empowers your earbuds to obey your every command, making your audio journey seamless and enchanting."
-    },
-    {
-      heading: "Artistic Mesh Guard",
-      description: "Shielded by artful mesh screens, our speakers remain untarnished, keeping your listening experience pristine."
-    }
-  ];
+  // const materialListData = [
+  //   {
+  //     heading: "Precision-Crafted Polymers",
+  //     description: "Our earbuds are meticulously molded from high-quality plastics, ensuring a blend of elegance, comfort, and resilience that's second to none."
+  //   },
+  //   {
+  //     heading: "Luxurious Silicone Harmony",
+  //     description: "Our uniquely engineered ear tips are cocooned in plush silicone, delivering an opulent embrace for your ears, ensuring an unrivaled fit and exquisite audio experience."
+  //   },
+  //   {
+  //     heading: "Rubberized Cables",
+  //     description: "Experience the unparalleled freedom of movement with our flexible rubber cables that promise durability without compromise."
+  //   },
+  //   {
+  //     heading: "Enhanced Comfort Sensors",
+  //     description: "A touch of magic in the form of built-in microphones and sensors empowers your earbuds to obey your every command, making your audio journey seamless and enchanting."
+  //   },
+  //   {
+  //     heading: "Artistic Mesh Guard",
+  //     description: "Shielded by artful mesh screens, our speakers remain untarnished, keeping your listening experience pristine."
+  //   }
+  // ];
 
-  //functions
+// FUNCTION TO CALL INFOBOXES FROM API
   function loadInfoBoxes() {
 
     fetch("https://swiftpixel.com/earbud/api/infoboxes").then(response => response.json()).then(infoBoxes => {
         console.log(infoBoxes);
 
         infoBoxes.forEach((infoBox, index) => {
-        let selected = document.querySelector(`#hotspot-${index + 1}`);
+          let selected = document.querySelector(`#hotspot-${index + 1}`);
 
-        const titleElement = document.createElement('h2');
-        titleElement.textContent = infoBox.heading;
+          const titleElement = document.createElement('h2');
+          titleElement.textContent = infoBox.heading;
 
-        const textElement = document.createElement('p');
-        textElement.textContent = infoBox.description;
+          const textElement = document.createElement('p');
+          textElement.textContent = infoBox.description;
 
-        selected.appendChild(titleElement);
-        selected.appendChild(textElement);
-      });
-    }).catch(error => {
+          selected.appendChild(titleElement);
+          selected.appendChild(textElement);
+        });
+      }).catch(error => {
       console.log(error);
 
 
 // Make and error message and put it here
 
     });
-    //make AJAX call here
-
-// BELOW was the code Marco provided, so have ported this into our AJAX call ABOVE^^ NOTE**** we had to adjust the elements that are called, (title was changed to heading, etc)
-
-    // infoBoxes.forEach((infoBox, index) => {
-    //   let selected = document.querySelector(`#hotspot-${index + 1}`);
-
-    //   const titleElement = document.createElement('h2');
-    //   titleElement.textContent = infoBox.title;
-
-    //   const textElement = document.createElement('p');
-    //   textElement.textContent = infoBox.text;
-
-    //   selected.appendChild(titleElement);
-    //   selected.appendChild(textElement);
-    // });
-  }
+  };
   loadInfoBoxes();
 
 
-// IN CLASS FUNCATION INSERTION
+// FUNCTION TO CALL MATERIAL LIST FROM API
 
 
   function loadMaterialInfo() {
 
+    fetch("https://swiftpixel.com/earbud/api/materials").then(response => response.json()).then(material => {
+      console.log(material);
+
+        material.forEach(material => {
+        // clone the template li w/ h3 and p inside
+        const clone = materialTemplate.content.cloneNode(true);
+        
+        // populate the clone template
+        const materialHeading = clone.querySelector(".material-heading");
+        materialHeading.textContent = material.heading;
+
+        const materialDescription = clone.querySelector(".material-desc");
+        materialDescription.textContent = material.description;
+
+        materialList.appendChild(clone);
+      });
+    }).catch(error => {
+      console.log(error);
 
 
 
 
-// make AJAX Call HERE to load materail info from the provided AJAX link 
-// add LOADER in HTML write code to show it here
-
-
-
-    materialListData.forEach(material => {
-      // clone the template li w/ h3 and p inside
-      const clone = materialTemplate.content.cloneNode(true);
-      
-      // populate the clone template
-      const materialHeading = clone.querySelector(".material-heading");
-      materialHeading.textContent = material.heading;
-
-      const materialDescription = clone.querySelector(".material-desc");
-      materialDescription.textContent = material.description;
-
-
-
-// hide the LOADER
-
-
-      materialList.appendChild(clone);
     });
-  }
-
+  };
   loadMaterialInfo();
   
 // NOTE *** For homework, mobile strategy (recommended still image), should have a loading bar, materials list cannot be in an array, must be pulled from API, failed to load error message
